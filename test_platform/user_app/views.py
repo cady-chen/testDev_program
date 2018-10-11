053700f148d6465c50b9202e5cefd4faa4ecb471
cady-chen <68182548@qq.com>
2018-10-11 11:17:24 +0800

    添加项目模块表

--- test_platform/user_app/views.py
+++ test_platform/user_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
-from user_app.models import ProjectInfo
+from user_app.models import Project,Module
 
 # Create your views here.
 #主要代码逻辑
@@ -34,7 +34,7 @@ def login_action(request):
 
 @login_required #判断用户是否已经登录，如果未登录，在浏览器中直接输入project_manage的地址，不让访问
 def project_manage(request):
-    project_list = ProjectInfo.objects.all()
+    project_list = Project.objects.all()
     username = request.session.get('user1', '')
     return render(request, "project_manage.html", {"user": username, "projects": project_list})
 

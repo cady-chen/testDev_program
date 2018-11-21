b3cc6950d9fcd0b78c1d8e0126be8d8d3d71b32b
cady-chen <68182548@qq.com>
2018-11-21 17:30:17 +0800

    重构接口代码，实现一些参数判断

---
+++ test_platform/interface_app/views/testcase_api.py
@@ -0,0 +1,129 @@
+import json
+import requests
+from test_platform import common
+from interface_app.models import TestCase
+from project_app.models import Project,Module
+
+"""
+说明：该文件串的接口由前段JS调用，返回JSON格式数据。
+"""
+
+
+def get_project_list(request):
+    """
+    获取项目模块列表
+    :param request:
+    :return: 项目接口列表
+    """
+    if request.method == "GET":
+        project_list = Project.objects.all()
+        data_list = []
+        for project in project_list:
+            project_dict = {
+                "name": project.name
+            }
+            module_list = Module.objects.filter(project_id = project.id)
+            if len(module_list) != 0:
+                module_name = []
+                for module in module_list:
+                    module_name.append(module.name)
+                project_dict["moduleList"] = module_name
+                data_list.append(project_dict)
+
+        return common.response_succeed(data=data_list)
+    else:
+        return common.response_failed("请求方法错误！")
+
+
+def api_debug(request):
+    """
+    HTTP接口调试
+    :param request:
+    :return: 接口调用结果
+    """
+    if request.method == "POST":
+        url = request.POST.get("req_url", "")
+        method = request.POST.get("req_method", "")
+        type = request.POST.get("req_type", "")
+        parameter = request.POST.get("req_parameter", "")
+
+        if url == "" or method == "" or type == "":
+            return common.response_failed("必传参数为空！")
+
+        if method == "get":
+            if type == "form":
+                r = requests.get(url, params = parameter)
+            else:
+                return common.response_failed("参数类型错误！")
+
+        if method == "post":
+            if type == "form":
+                r = requests.post(url, data=parameter)
+            elif type == "json":
+                r = requests.post(url, json=parameter)
+            else:
+                return common.response_failed("参数类型错误！")
+
+        return common.response_succeed(data = r.text)
+    else:
+        return common.response_failed("请求方法错误！")
+
+
+def api_assert(request):
+    """
+    对测试用例的断言进行验证
+    :param request:
+    :return:
+    """
+    if request.method == "POST":
+        result_text = request.POST.get("result", "")
+        assert_text = request.POST.get("assert", "")
+
+        if result_text == ""  or assert_text == "":
+            return common.response_failed("验证的数据不能为空！")
+
+        try:
+            assert assert_text in result_text
+        except AssertionError:
+            return common.response_failed("验证失败！")
+        else:
+            return common.response_succeed("验证成功！")
+    else:
+        return common.response_failed("请求方法错误！")
+
+
+def save_case(request):
+    """
+    保存接口测试用例
+    :param request:
+    :return:
+    """
+    if request.method == "POST":
+        name = request.POST.get("name", "")
+        url = request.POST.get("req_url", "")
+        method = request.POST.get("req_method", "")
+        parameter = request.POST.get("req_parameter", "")
+        req_type = request.POST.get("req_type", "")
+        header = request.POST.get("header", "")
+        module_name = request.POST.get("module", "")
+        assert_text = request.POST.get("assert_text", "")
+
+        if url == "" or method == "" or req_type == "" or module_name == "" or assert_text == "":
+            return common.response_failed("必传参数为空")
+
+        if parameter == "":
+            parameter = "{}"
+        if header == "":
+            header = "{}"
+
+        module_obj = Module.objects.get(name=module_name)
+        case = TestCase.objects.create(name=name, module=module_obj, url=url,
+                                   req_method = method, req_header = header,
+                                   req_type = req_type, req_parameter = parameter,
+                                   respose_assert = assert_text)
+        if case is not None:
+            return common.response_succeed("保存成功！")
+
+    else:
+        return common.response_failed("请求方法错误！")
+
---
+++ test_platform/interface_app/views/testcase_views.py
@@ -0,0 +1,102 @@
+import json
+import requests
+from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.http import HttpResponse
+from interface_app.models import TestCase
+from interface_app.forms import TestCaseForm
+
+
+# Create your views here.
+
+
+#获取用例例表
+def case_manage(request):
+    testcases = TestCase.objects.all()
+    #定义页面上展示5条用例
+    paginator = Paginator(testcases, 5)
+    page = request.GET.get('page')
+    try:
+        contacts = paginator.page(page)
+    except PageNotAnInteger:
+        #如果页数不是整型，取第一页
+        contacts = paginator.page(1)
+    except EmptyPage:
+        #如果页数超出查询范围，取最后一页
+        contacts = paginator.page(paginator.num_pages)
+
+
+    if request.method == 'GET':
+        return render(request, "case_manage.html",
+                      {"type": "list",
+                       "testcases": contacts,
+                       })
+    else:
+        return HttpResponse("404")
+
+#根据用例名称搜索
+def search_case_name(request):
+
+    if request.method == "GET":
+        case_name = request.GET.get('case_name', "")
+        cases = TestCase.objects.filter(name__contains=case_name)
+        paginator = Paginator(cases, 5)
+        page = request.GET.get('page')
+        try:
+            contacts = paginator.page(page)
+        except PageNotAnInteger:
+            # 如果页数不是整型，取第一页
+            contacts = paginator.page(1)
+        except EmptyPage:
+            # 如果页数超出查询范围，取最后一页
+            contacts = paginator.page(paginator.num_pages)
+
+        return render(request, "case_manage.html",{
+            "type": "list",
+            "testcases": contacts,
+            "case_name": case_name,
+        })
+    else:
+        return HttpResponse("404")
+
+
+#创建接口测试用例
+def add_case(request):
+    if request.method == "GET":
+        return render(request, "add_case.html", {
+            "type": "add"
+        })
+    else:
+        return HttpResponse("404")
+
+#编辑接口测试用例
+def debug_case(request, cid):
+    if request.method == "GET":
+        return render(request, "debug_case.html",{
+            "type": "debug"
+        })
+    else:
+        return HttpResponse("404")
+
+
+def debug(request):
+    if request.method == "GET":
+        form = TestCaseForm()
+        return render(request, "api_debug.html", {"form": form, "type": "debug"})
+    else:
+        return HttpResponse("404")
+
+#调试接口
+def api_debug(request):
+    if request.method == 'POST':
+        url = request.POST.get("req_url")
+        method = request.POST.get("req_method")
+        parameter = request.POST.get("req_parameter")
+        if method == "get":
+            r = requests.get(url, params=parameter)
+        if method == "post":
+            r = requests.post(url, json=parameter)
+        return HttpResponse(r.text)
+    else:
+        return render(request, "api_debug.html", {"type": "debug"})
+

dc65852b40c376933ba0ed52bc6442d237377342
cady-chen <68182548@qq.com>
2018-11-21 23:37:36 +0800

    update module code

--- test_platform/interface_app/views/testcase_api.py
+++ test_platform/interface_app/views/testcase_api.py
@@ -127,3 +127,133 @@ def save_case(request):
     else:
         return common.response_failed("请求方法错误！")
 
+
+def api_assert(request):
+    """
+    对测试用例的断言进行验证
+    :param request:
+    :return:
+    """
+    if request.method == "POST":
+        result_text = request.POST.get("result", "")
+        assert_text = request.POST.get("assert", "")
+
+        if result_text == "" or assert_text == "":
+            return common.response_failed("验证的数据不能为空！")
+
+        try:
+            assert assert_text in result_text
+        except AssertionError:
+            return common.response_failed("验证失败！")
+        else:
+            return common.response_succeed("验证成功！")
+    else:
+        return common.response_failed("请求方法错误！")
+
+
+def save_case(request):
+    """
+    保存接口测试用例
+    :param request:
+    :return:
+    """
+    if request.method == "POST":
+        name = request.POST.get("name", "")
+        url = request.POST.get("req_url", "")
+        method = request.POST.get("req_method", "")
+        parameter = request.POST.get("req_parameter", "")
+        req_type = request.POST.get("req_type", "")
+        header = request.POST.get("header", "")
+        module_name = request.POST.get("module", "")
+        assert_text = request.POST.get("assert_text", "")
+
+        if url == "" or method == "" or req_type == "" or module_name == "" or assert_text == "":
+            return common.response_failed("必传参数为空！")
+        if parameter == "":
+            parameter = "{}"
+
+        if header == "":
+            header = "{}"
+
+        module_obj = Module.objects.get(name=module_name)
+        case = TestCase.objects.create(name=name, module=module_obj, url=url,
+                                       req_method=method, req_header=header,
+                                       req_type=req_type, req_parameter=parameter,
+                                       response_assert=assert_text)
+        if case is not None:
+            return common.response_succeed("保存成功！")
+
+    else:
+        return common.response_failed("请求方法错误！")
+
+
+def update_case(request):
+    """
+    更新接口测试用例
+    :param request:
+    :return:
+    """
+    if request.method == "POST":
+        case_id = request.POST.get("cid", "")
+        name = request.POST.get("name", "")
+        url = request.POST.get("req_url", "")
+        method = request.POST.get("req_method", "")
+        parameter = request.POST.get("req_parameter", "")
+        req_type = request.POST.get("req_type", "")
+        header = request.POST.get("header", "")
+        module_name = request.POST.get("module", "")
+        assert_text = request.POST.get("assert_text", "")
+
+        if url == "" or method == "" or req_type == "" or module_name == "" or assert_text == "":
+            return common.response_failed("必传参数为空！")
+        if parameter == "":
+            parameter = "{}"
+
+        if header == "":
+            header = "{}"
+
+        module_obj = Module.objects.get(name=module_name)
+        case_obj = TestCase.objects.filter(id=case_id).update(module=module_obj, name=name, url=url,
+                                       req_method=method, req_header=header,
+                                       req_type=req_type, req_parameter=parameter,
+                                       response_assert=assert_text)
+        if case_obj == 1:
+            return common.response_succeed("更新成功！")
+        else:
+            return common.response_failed("更新失败！")
+    else:
+        return common.response_failed("请求方法错误！")
+
+
+def get_case_info(request):
+    """
+    获取接口数据
+    :param request:
+    :return:
+    """
+    if request.method == "POST":
+        case_id = request.POST.get("caseId", "")
+        if case_id == "":
+            return common.response_failed("用例id为空！")
+
+        case_obj = TestCase.objects.get(pk=case_id)
+        module_obj = Module.objects.get(id=case_obj.module_id)
+        module_name = module_obj.name
+
+        project_name = Project.objects.get(id=module_obj.project_id).name
+
+        case_info = {
+            "modulename": module_name,
+            "projectName": project_name,
+            "name": case_obj.name,
+            "url": case_obj.url,
+            "reqMethod": case_obj.req_method,
+            "reqType": case_obj.req_type,
+            "reqHeader": case_obj.req_header,
+            "reqParameter": case_obj.req_parameter,
+            "assertText":case_obj.response_assert,
+        }
+
+        return common.response_succeed(data=case_info)
+    else:
+        return common.response_failed("请求方法错误！")
---
+++ test_platform/static/js/myCase.js
@@ -0,0 +1,37 @@
+// 获取指定case_id的用例信息
+var CaseInit = function (case_id) {
+
+    function getCaseInfo() {
+        // 获取某个用例的信息
+        $.post("/interface/get_case_info/", {
+            "caseId": case_id,
+        }, function (resp) {
+            if (resp.success === "true") {
+                let result = resp.data;
+                console.log("结果", result);
+                document.getElementById("req_name").value = result.name;
+                document.getElementById("req_url").value = result.url;
+                document.getElementById("req_header").value = result.reqHeader;
+                document.getElementById("req_parameter").value = result.reqParameter;
+                document.getElementById("assert_text").value = result.assertText;
+
+                if (result.reqMethod === "post"){
+                    document.getElementById("post").setAttribute("checked", "")
+                }
+
+                if (result.reqType === "json"){
+                    document.getElementById("json").setAttribute("checked", "")
+                }
+
+                // 初始化菜单
+                ProjectInit('project_name', 'module_name', result.projectName, result.moduleName);
+
+            }else {
+                window.alert(resp.message);
+            }
+        });
+    }
+    // 调用getCaseInfo函数
+    getCaseInfo();
+
+};
